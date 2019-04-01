b80963633a4a2f83aa9131faca116394c82ce0d1
d-toth1 <dtoth1@nd.edu>
2019-04-01 11:17:50 -0400

    imports.py

---
+++ imports.py
@@ -0,0 +1,8 @@
+# List of imports to save time during scripting
+# Type: from imports import * to import all of these libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import scipy.stats as stat
+import statsmodels.api as sm
